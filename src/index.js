af7289947d7ae22258477fa1014b6930d3a7eb09
DmitriyGryaznov <154207588+DmitriyGryaznov@users.noreply.github.com>
2024-10-01 11:38:24 +0300

    2

--- src/index.js
+++ src/index.js
@@ -5,26 +5,6 @@ import { Alert } from 'antd';
 import { Online, Offline } from 'react-detect-offline';
 
 import App from './App';
-
-// const Online = () => {
-//   return (
-//     <div className="online">
-//       {/* Render the App component here */}
-//       <App className="app" />
-//     </div>
-//   );
-// };
-
-// // Define the Offline component
-// const Offline = () => {
-//   return (
-//     <div className="offline">
-//       {/* Render the alert message here */}
-//       <Alert type="error" message={`Можно было бы навести суету, но у тебя нет инета :(`} />
-//     </div>
-//   );
-// };
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
@@ -38,7 +18,3 @@ root.render(
     </Offline>
   </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
